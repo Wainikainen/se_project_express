6fd209e95235f1a912e32af5eb1eb5d6ed4297cd
adam wainkainen <awainikainen@gmail.com>
2025-09-12 23:07:16 -0400

    added other routes for functions

---
+++ routes/clothingItems.js
@@ -0,0 +1,11 @@
+const router = require("express").Router()
+const  { getItems, createItem, deleteItem, likeItem, dislikeItem } = require("../controllers/clothingitems");
+
+router.post("/", createItem );
+router.get("/", getItems);
+router.delete("/:itemId", deleteItem);
+router.put("/:itemId/likes", likeItem);
+router.delete("/:itemId/likes", dislikeItem);
+
+
+module.exports = router;
