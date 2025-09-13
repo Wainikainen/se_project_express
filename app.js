d40de9ecd147982bc7480c0c63e553d34c1cfacb
adam wainkainen <awainikainen@gmail.com>
2025-09-12 23:04:41 -0400

    added root imports for app and hardcoded id for testing

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const { PORT = 3001 } = process.env
 const app = express();
 const indexRouter = require("./routes/index");
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: "68c4d40991ded55217d9cf39"
+  };
+  next();
+});
+
+app.use(express.json());
+
 mongoose.connect(
   'mongodb://127.0.0.1:27017/wtwr_db'
 );
 
 app.use("/", indexRouter)
 
-app.listen(PORT, () => {
-
-});
+app.listen(PORT, () => {});
