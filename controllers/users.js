654eed15a5fa2a4538d400c426a40420a1ffdbc4
adam wainkainen <awainikainen@gmail.com>
2025-09-16 21:22:20 -0400

    added login

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,59 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
-const { BAD_REQUEST, NOT_FOUND, SERVER_ERROR } = require('../utils/errors');
+const {JWT_SECRET} = require("../utils/config");
+const {
+  BAD_REQUEST,
+  NOT_FOUND,
+  SERVER_ERROR,
+  CONFLICT_ERROR,
+  UNAUTH_ERROR
+} = require("../utils/errors");
 
 const createUser = (req, res) => {
-  const { name, avatar } = req.body;
-  User.create({ name, avatar })
-    .then((user) => res.status(201).send(user))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        return res.status(BAD_REQUEST).send({ message: err.message });
-      }
+  const { name, avatar, email, password } = req.body;
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    if (err) {
       return res.status(SERVER_ERROR).send({ message: err.message });
+    }
+    User.create({ name, avatar, email, password: hashedPassword })
+      .then((user) => res.status(201).send(user))
+      .catch((err) => {
+        console.error(err);
+        if (err.code === 11000) {
+          return res.status(CONFLICT_ERROR).send({ message: "Email already exists" });
+        }
+        if (err.name === "ValidationError") {
+          return res.status(BAD_REQUEST).send({ message: err.message });
+        }
+        return res.status(SERVER_ERROR).send({ message: err.message });
+      });
+  });
+};
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Incorrect email or password'));
+      }
+      return bcrypt.compare(password, user.password);
+    })
+    .then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error('Incorrect email or password'));
+      }
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+         expiresIn: "7d"
+      });
+      res.status(200).send({token});
+
+    })
+    .catch((err) => {
+      res
+        .status(UNAUTH_ERROR)
+        .send({ message: err.message });
     });
 };
 
@@ -33,4 +76,4 @@ const getUser = (req, res) => {
     });
 };
 
-module.exports = { getUsers, getUser, createUser };
+module.exports = { getUsers, getUser, createUser, login };
