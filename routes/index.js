30336e76090e65910df2b411e3d7939eeaea2414
adam wainkainen <awainikainen@gmail.com>
2025-09-16 22:34:02 -0400

    updated sign up and login routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
+const { login, createUser } = require("../controllers/users");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
+router.post("/signup", createUser);
+router.post("/login", login);
+
+
 router.use((req, res) => {
   res.status(404).send({
     message: "Requested resource not found",
   });
 });
+
 module.exports = router;
