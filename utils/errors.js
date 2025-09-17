595663ae1e26146ef0fc27d137bfe82d2ffc13d2
adam wainkainen <awainikainen@gmail.com>
2025-09-16 21:23:11 -0400

    added unauthorized 401

--- utils/errors.js
+++ utils/errors.js
@@ -1,9 +1,13 @@
 const BAD_REQUEST = 400;
 const NOT_FOUND = 404;
 const SERVER_ERROR = 500;
+const CONFLICT_ERROR =  409;
+const UNAUTH_ERROR = 401;
 
 module.exports = {
   BAD_REQUEST,
   NOT_FOUND,
   SERVER_ERROR,
+  CONFLICT_ERROR,
+  UNAUTH_ERROR
 };
