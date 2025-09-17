c84da65c87646fdc3bfd327062033781c91a2241
adam wainkainen <awainikainen@gmail.com>
2025-09-17 14:00:03 -0400

    updated check for login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -29,7 +30,9 @@ const userSchema = new mongoose.Schema({
     },
   },
   password: {
+    type: String,
     required: true,
+    select: false,
     validate: {
       validator(value) {
         return validator.isStrongPassword(value);
@@ -44,17 +47,19 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(
   email,
   password
 ) {
-  return this.findOne({ email }).then((user) => {
-    if (!user) {
-      return Promise.reject(new Error("Incorrect email or password!"));
-    }
-    return bcrypt.compare(password, user.password).then((matched) => {
-      if (!matched) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
         return Promise.reject(new Error("Incorrect email or password!"));
       }
-      return user;
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password!"));
+        }
+        return user;
+      });
     });
-  });
 };
 
 module.exports = mongoose.model("User", userSchema);
