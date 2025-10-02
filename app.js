95d921f1b291426307b94d66d7c5462240747cd3
adam wainkainen <awainikainen@gmail.com>
2025-10-02 15:15:53 -0400

    added separate logger file for errors

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const cors = require("cors");
 const indexRouter = require("./routes/index");
 const { errorHandler } = require("./middlewares/error");
 const { errors: celebrateErrors } = require("celebrate");
-
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 const { PORT = 3001 } = process.env;
 const app = express();
+
 app.use(express.json());
 
 mongoose
@@ -21,7 +22,11 @@ mongoose
 
 app.use(cors());
 
+app.use(requestLogger);
+
 app.use("/", indexRouter);
+
+app.use(errorLogger);
 
 app.use(celebrateErrors());
 
